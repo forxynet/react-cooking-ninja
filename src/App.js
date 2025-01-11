2554d8c9ef63625c2351e89c235d5a77087ca7da
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-12 02:01:26 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// navbar
+import Navbar from './components/Navbar';
+
+// styles
 import './App.css'
+
+//pages
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
 
 function App() {
   return (
     <div className="App">
-      
-    </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div >
   );
 }
 
