eb3ad5abd071c9ea29cb73dc7ce46c8a216f09b3
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-12 13:19:48 +0300

    added recipe js and css

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Recipe.css'
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url)
+
   return (
-    <div>
-      Recipe
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p>Loading..</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(item => <li key={item}>{item}</li>)}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+      )}
     </div>
   )
 }
